b0c49c825859e1b9ebe44a60e63cbf80d55b807e
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-07-26 17:46:21 +0900

    feat: 🎸 Add associations to CharacterCharacterNickname

--- app/models/character_nickname.rb
+++ app/models/character_nickname.rb
@@ -1,2 +1,4 @@
 class CharacterNickname < ApplicationRecord
+  has_many :characters_character_nicknames, dependent: :destroy
+  has_many :characters, through: :characters_character_nicknames
 end
--- app/models/characters_character_nickname.rb
+++ app/models/characters_character_nickname.rb
@@ -1,2 +1,4 @@
 class CharactersCharacterNickname < ApplicationRecord
+  belongs_to :character
+  belongs_to :character_nickname
 end
